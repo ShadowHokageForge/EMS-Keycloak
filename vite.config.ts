bab96b849dfc2f398a764bfb0ea132e8438b3bc3
RudhraBharathy <bharathyganeshan@gmail.com>
2024-10-13 00:55:04 +0530

    Changes in the keycloakify-starter kit files

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { keycloakify } from "keycloakify/vite-plugin";
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
